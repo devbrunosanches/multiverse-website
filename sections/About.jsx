752e8fd0eaaf0d345a89094e0b0c26f3f4207424
devbrunosanches <dev.brunosanches@gmail.com>
2023-05-19 15:39:49 -0300

    about insight and world sections

--- sections/About.jsx
+++ sections/About.jsx
@@ -24,7 +24,6 @@ const About = () => (
         It invites us to ponder the boundless 
         <span className="font-extrabold text-white"> nature of the cosmos.</span>
       </motion.p>
-      <motion.img variants={fadeIn("up", "tween", 0.3, 1)} src="/arrow-down.svg" alt="arrow down" className="w-[18px] h-[28px] object-contain mt-[28px]" />
     </motion.div>
   </section>
 );
