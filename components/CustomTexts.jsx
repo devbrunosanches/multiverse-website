56a3b291a35e78f02f87f759081dd0bdc1c1612d
devbrunosanches <dev.brunosanches@gmail.com>
2023-05-19 11:40:48 -0300

    explore multiverse section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { textContainer, textVariant2 } from "../utils/motion";
+import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p variants={textContainer} className={`font-normal text-[14px] text-secondary-white ${textStyles}`}>
@@ -14,7 +14,7 @@ export const TypingText = ({ title, textStyles }) => (
 );
 
 export const TitleText = ({ title, textStyles }) => (
-  <h2>T
-    itle Text
-  </h2>
+  <motion.h2 variants={textVariant2} initial="hidden" whileInView="show" className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}>
+    {title}
+  </motion.h2>
 );
